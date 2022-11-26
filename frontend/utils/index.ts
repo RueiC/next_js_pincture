bbd9f8a70a226c25285b5c762e430cea2666fe43
睿宸 <user@userde-MacBook-Pro-2.local>
2022-11-27 03:22:59 +0800

    bug fix

--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
-import jwtDecode from "jwt-decode";
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
 export const createOrGetUser = async (res: any) => {
   const decoded: { name: string; picture: string; sub: string } = jwtDecode(
-    res.credential
+    res.credential,
   );
 
   const { name, picture, sub } = decoded;
 
   const user = {
     _id: sub,
-    _type: "user",
+    _type: 'user',
     userName: name,
     image: picture,
   };
@@ -18,9 +18,9 @@ export const createOrGetUser = async (res: any) => {
   const env = process.env.NODE_ENV;
   console.log(env);
 
-  if (env === "development") {
+  if (env === 'development') {
     await axios.post(`http://localhost:3000/api/auth`, user);
-  } else if (env === "production") {
-    await axios.post(`http://react-pincture.vercel.app/api/auth`, user);
+  } else if (env === 'production') {
+    await axios.post(`http://next-js-pincture.vercel.app/api/auth`, user);
   }
 };
